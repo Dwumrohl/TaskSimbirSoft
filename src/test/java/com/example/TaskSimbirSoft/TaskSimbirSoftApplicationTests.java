0d292af1d84a0461ef5993485f562b094c730494
Dwumrohl <81928766+Dwumrohl@users.noreply.github.com>
2023-02-25 21:42:26 +0400

    Initial commit

---
+++ src/test/java/com/example/TaskSimbirSoft/TaskSimbirSoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TaskSimbirSoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSimbirSoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
